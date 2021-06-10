6d3488754abed0aa16dc26166d200dab865b85b7
Dekaver <alwiwahyudi8@gmail.com>
2021-06-11 01:30:09 +0700

    fix

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def search():
     docs , waktu = ranking(query, main.df)
     return render_template('search.html', 
         docs=docs, 
-        dataframe=main.dataframe, 
+        dataframe=main.df, 
         waktu=waktu, 
         query=query,
         url=main.url,)
